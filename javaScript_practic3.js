443dcc788c7950e0c0bdabc877cb289d9f5f5b18
esteban08-S <soteloesteban200@gmail.com>
2025-03-17 17:40:39 -0300

    ejercicio 3

---
+++ javaScript_practic3.js
@@ -0,0 +1,17 @@
+function eje2() {
+    var numero1 = 5;
+    var numero2 = 8;
+    if(numero1<numero2) {
+    console.log("numero1 no es mayor que numero2");
+    }
+    if(numero2+) {
+    console.log("numero2 es positivo");
+    }
+    if() {
+    console.log("numero1 es negativo o distinto de cero");
+    }
+    if(...) {
+    console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que
+    numero2");  
+}
+
